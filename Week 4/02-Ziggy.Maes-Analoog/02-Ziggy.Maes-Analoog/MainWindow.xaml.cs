353398fe0f7579a02b163baedd0f090e29020705
Ziggy Maes <ziggymaes@telenet.be>
2015-03-05 10:16:00 +0100

    Werkende sensoren.

--- Week 4/02-Ziggy.Maes-Analoog/02-Ziggy.Maes-Analoog/MainWindow.xaml.cs
+++ Week 4/02-Ziggy.Maes-Analoog/02-Ziggy.Maes-Analoog/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,63 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using USB_DAQBoard;
 
 namespace _02_Ziggy.Maes_Analoog
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
     public partial class MainWindow : Window
     {
+        BackgroundWorker bw = new BackgroundWorker();
+
         public MainWindow()
         {
             InitializeComponent();
+            MPUSB.OpenMPUSBDevice();
+            bw.WorkerSupportsCancellation = true;
+            bw.WorkerReportsProgress = true;
+
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged +=  new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+
+            bw.RunWorkerAsync();
         }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            System.Threading.Thread.Sleep(50);
+            worker.ReportProgress(100);
+        }
+
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var valbar1 = MPUSB.ReadAnalogIn(0);
+            //Console.WriteLine(valbar1);
+            bar1.Value = valbar1;
+
+            var valbar2 = MPUSB.ReadAnalogIn(1);
+            Console.WriteLine(valbar2);
+            bar2.Value = valbar2;
+
+            var valbar3 = MPUSB.ReadAnalogIn(2);
+            Console.WriteLine(valbar3);
+            bar3.Value = valbar3;
+
+            var valbar4 = MPUSB.ReadAnalogIn(3);
+            Console.WriteLine(valbar4);
+            bar4.Value = valbar4;
+
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bw.RunWorkerAsync();
+        }
+
     }
 }
